97bc7b28e6da9813f78cd5763b80b1b5288e79b8
DorinC263 <139365566+DorinC263@users.noreply.github.com>
2023-08-25 15:40:04 +0200

    fix #19 Split the Winning Lies into Sub-Methods for easy readability

--- LogicMethods.cs
+++ LogicMethods.cs
@@ -21,84 +21,98 @@ namespace SlotMachine2
 
         public static bool CheckWinningLines(int[,] grid, int wager, out int winAmount)
         {
-            bool winningLine = false;
             winAmount = 0;
 
             if(wager >= VERTICAL_LINES)
             {
-                // Check vertical lines
-                for (int col = 0; col < COLUMN_COUNT; col++)
-                {
-                    winningLine = false;
-                    for (int row = 0; row < ROW_COUNT - 1; row++)
-                    {
-                        if (grid[row, col] != grid[row + 1, col])
-                        {
-                            winningLine = true;
-                            break;
-                        }
-                    }
-                    if (!winningLine)
-                    {
-                        winAmount += wager; // Add wager to winAmount for each vertical line
-                    }
-                }
+                CheckVerticalLines(grid, wager, ref winAmount);
             }
             
             if(wager >= HORIZONTAL_LINES)
             {
-                // Check horizontal lines
-                for (int row = 0; row < ROW_COUNT; row++)
-                {
-                    winningLine = false;
-                    for (int col = 0; col < COLUMN_COUNT - 1; col++)
-                    {
-                        if (grid[row, col] != grid[row, col + 1])
-                        {
-                            winningLine = true;
-                            break;
-                        }
-                    }
-                    if (!winningLine)
-                    {
-                        winAmount += wager; // Add wager to winAmount for each horizontal line
-                    }
-                }
+                CheckHorizontalLines(grid, wager, ref winAmount);
             }
             
             if(wager >= ALL_LINES)
             {
-                // Check diagonal lines
-                winningLine = false;
-                for (int i = 0; i < ROW_COUNT - 1; i++)
-                {
-                    if (grid[i, i] != grid[i + 1, i + 1])
-                    {
-                        winningLine = true;
-                        break;
-                    }
-                }
-                if (!winningLine)
-                {
-                    winAmount += wager; // Add wager to winAmount for diagonal line
-                }
-
-                winningLine = false;
-                for (int i = 0; i < ROW_COUNT - 1; i++)
-                {
-                    if (grid[i, COLUMN_COUNT - 1 - i] != grid[i + 1, COLUMN_COUNT - 2 - i])
-                    {
-                        winningLine = true;
-                        break;
-                    }
-                }
-                if (!winningLine)
-                {
-                    winAmount += wager; // Add wager to winAmount for diagonal line
-                }
+                CheckDiagonalLines(grid, wager, ref winAmount);
             }          
 
             return winAmount > 0; // Return true if winAmount is greater than 0
+        }
+
+        public static void CheckVerticalLines(int[,] grid,int wager,ref int winAmount)
+        {
+            // Check vertical lines
+            for (int col = 0; col < COLUMN_COUNT; col++)
+            {
+                bool winningLine = false;
+                for (int row = 0; row < ROW_COUNT - 1; row++)
+                {
+                    if (grid[row, col] != grid[row + 1, col])
+                    {
+                        winningLine = true;
+                        break;
+                    }
+                }
+                if (!winningLine)
+                {
+                    winAmount += wager; // Add wager to winAmount for each vertical line
+                }
+            }
+        }
+
+        public static void CheckHorizontalLines(int[,] grid, int wager,ref int winAmount)
+        {
+            // Check horizontal lines
+            for (int row = 0; row < ROW_COUNT; row++)
+            {
+                bool winningLine = false;
+                for (int col = 0; col < COLUMN_COUNT - 1; col++)
+                {
+                    if (grid[row, col] != grid[row, col + 1])
+                    {
+                        winningLine = true;
+                        break;
+                    }
+                }
+                if (!winningLine)
+                {
+                    winAmount += wager; // Add wager to winAmount for each horizontal line
+                }
+            }
+        }
+
+        public static void CheckDiagonalLines(int[,]grid, int wager,ref int winAmount)
+        {
+            // Check diagonal lines
+            bool winningLine = false;
+            for (int i = 0; i < ROW_COUNT - 1; i++)
+            {
+                if (grid[i, i] != grid[i + 1, i + 1])
+                {
+                    winningLine = true;
+                    break;
+                }
+            }
+            if (!winningLine)
+            {
+                winAmount += wager; // Add wager to winAmount for diagonal line
+            }
+
+            winningLine = false;
+            for (int i = 0; i < ROW_COUNT - 1; i++)
+            {
+                if (grid[i, COLUMN_COUNT - 1 - i] != grid[i + 1, COLUMN_COUNT - 2 - i])
+                {
+                    winningLine = true;
+                    break;
+                }
+            }
+            if (!winningLine)
+            {
+                winAmount += wager; // Add wager to winAmount for diagonal line
+            }
         }
     }
 }
